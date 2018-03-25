6b83fa406e98650e72e10775972118539f085e1b
queeniexiang <wxiang1@stuy.edu>
2018-03-24 20:06:11 -0400

    Added more

--- homepage.js
+++ homepage.js
@@ -1,11 +1,16 @@
-var data = [4, 8, 15, 16, 23, 42];
+var chart_1962 = d3.select(".chart_1962");
+//console.log(chart_1962);
+var chart_2023 = d3.select(".chart_2023");
+//console.log(chart_2023);
+
+var 1962_data = [];
+var 2023_data = [];
+
+//Grabs data from the csv file and puts it into the 1962 or 2023 data array
+//1962 is index 0
+//2023 is index length-1 
+var grabData = function() {
+}
 
 
-var 2016_chart = d3.select(".2016") 
-var chart = d3.select(".chart");
-var bar = chart.selectAll("div");
-var barUpdate = bar.data(data);
-var barEnter = barUpdate.enter().append("div");
-
-barEnter.text(function(d) { return d; });
 
